33e6ab439f8cdd7e13323bcbbffce339d0279a44
Moozon <muzhenweizh@gmail.com>
2023-11-08 15:47:42 +0800

    feat: src + test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,56 @@
-export const one = 1
-export const two = 2
+class Node {
+  value: any
+  next: Node | null = null
+
+  constructor(value: any) {
+    this.value = value
+  }
+}
+
+export class Queue {
+  #head: Node | null = null
+  #tail: Node | null = null
+  #size: number = 0
+
+  clear() {
+    this.#head = null
+    this.#tail = null
+    this.#size = 0
+  }
+
+  enqueue(value: any) {
+    const newNode = new Node(value)
+    if (!this.#head) {
+      this.#head = newNode
+      this.#tail = newNode
+    }
+    else {
+      this.#tail!.next = newNode
+      this.#tail = newNode
+    }
+    this.#size++
+  }
+
+  dequeue() {
+    if (!this.#head)
+      return
+
+    const oldHead = this.#head
+    this.#head = this.#head.next
+    this.#size--
+
+    return oldHead.value
+  }
+
+  get size() {
+    return this.#size
+  }
+
+  *[Symbol.iterator]() {
+    let cur = this.#head
+    while (cur) {
+      yield cur.value
+      cur = cur.next
+    }
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,64 @@
 import { describe, expect, it } from 'vitest'
+import { Queue } from '../src'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
-  })
+it('init', () => {
+  const q = new Queue()
+  expect(q.size).toBe(0)
+})
+
+it('.enqueue()', () => {
+  const q = new Queue()
+  q.enqueue('🦄')
+  expect(q.dequeue()).toBe('🦄')
+  q.enqueue('🌈')
+  q.enqueue('🌟')
+  expect(q.dequeue()).toBe('🌈')
+  expect(q.dequeue()).toBe('🌟')
+})
+
+it('.dequeue()', () => {
+  const q = new Queue()
+  expect(q.dequeue()).toBeUndefined()
+  expect(q.dequeue()).toBeUndefined()
+  expect(q.size).toBe(0)
+  q.enqueue('🦄')
+  expect(q.dequeue()).toBe('🦄')
+  expect(q.dequeue()).toBeUndefined()
+})
+
+it('.clear()', () => {
+  const q = new Queue()
+  q.clear()
+  q.enqueue(1)
+  q.clear()
+  expect(q.size).toBe(0)
+  q.enqueue(1)
+  q.enqueue(2)
+  q.enqueue(3)
+  q.clear()
+  expect(q.size).toBe(0)
+})
+
+it('.size', () => {
+  const q = new Queue()
+  expect(q.size).toBe(0)
+  q.clear()
+  expect(q.size).toBe(0)
+  q.enqueue('🦄')
+  expect(q.size).toBe(1)
+  q.enqueue('🦄')
+  expect(q.size).toBe(2)
+  q.dequeue()
+  expect(q.size).toBe(1)
+  q.dequeue()
+  expect(q.size).toBe(0)
+  q.dequeue()
+  expect(q.size).toBe(0)
+})
+
+it('iterable', () => {
+  const queue = new Queue()
+  queue.enqueue('🦄')
+  queue.enqueue('🌈')
+  expect([...queue]).toEqual(['🦄', '🌈'])
 })
